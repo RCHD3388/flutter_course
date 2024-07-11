f12f80c0e3192f59f9c5824d7a56c33ba75f5004
RCHD88 <richardraferguy@gmail.com>
2024-07-11 23:43:14 +0700

    add initial style app

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -1,1 +1,13 @@
-// empty file
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello world!'),
+        ),
+      ),
+    ),
+  );
+}
