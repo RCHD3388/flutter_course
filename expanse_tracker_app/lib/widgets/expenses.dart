d33a752f9a795ea3a44aea225e11ad21eaef1272
RCHD3388 <richardraferguy@gmail.com>
2024-07-25 15:42:35 +0700

    add add expense modal

--- expanse_tracker_app/lib/widgets/expenses.dart
+++ expanse_tracker_app/lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expanse_tracker_app/widgets/expenses_list/expenses_list.dart';
 import 'package:expanse_tracker_app/models/expense.dart';
+import 'package:expanse_tracker_app/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -27,13 +28,23 @@ class _ExpensesState extends State<Expenses> {
     )
   ];
 
+  _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("flutter expense tracker"),
+        title: const Text("flutter expense"),
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
         ],
       ),
       body: Column(
