4143a8efdd480071a21d3b0e6920b75c225dbc80
RCHD88 <richardraferguy@gmail.com>
2024-07-30 01:21:22 +0700

    latest

--- meals/lib/widgets/meal_item_trait.dart
+++ meals/lib/widgets/meal_item_trait.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:meals/widgets/meal_item.dart';
 
 class MealItemTrait extends StatelessWidget {
   const MealItemTrait({super.key, required this.icon, required this.label});
