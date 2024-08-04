d5ffe86c751e091746e0a5205dd49b18f744457f
RCHD88 <richardraferguy@gmail.com>
2024-08-04 14:12:01 +0700

    meals application done

--- meals_app/lib/widgets/meal_item.dart
+++ meals_app/lib/widgets/meal_item.dart
@@ -34,12 +34,15 @@ class MealItem extends StatelessWidget {
         child: Stack(
           // stack back - front
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              fit: BoxFit.cover, // image cover
-              height: 200,
-              width: double.infinity,
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover, // image cover
+                height: 200,
+                width: double.infinity,
+              ),
             ), // faded image background
             Positioned(
               bottom: 0,
